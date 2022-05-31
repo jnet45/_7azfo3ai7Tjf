264acc088ec2a18a6793a4c9453e1a855646d5b9
jnet45 <81304762+jnet45@users.noreply.github.com>
2022-05-31 17:49:22 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,29 @@
+input.onButtonPressed(Button.A, function () {
+    aa = 1
+})
+input.onButtonPressed(Button.B, function () {
+    aa = 0
+})
+let aa = 0
 basic.showLeds(`
     # # # # #
     # . . . #
     # . . . #
-    # . . . #
+    # . # . #
     # # # # #
     `)
+aa = 0
 basic.forever(function () {
-    if (pins.analogReadPin(AnalogPin.P0) < 100) {
-        pins.analogWritePin(AnalogPin.P12, 512)
-        pins.analogWritePin(AnalogPin.P16, 0)
+    if (aa == 1) {
+        if (pins.analogReadPin(AnalogPin.P0) < 100) {
+            pins.analogWritePin(AnalogPin.P8, 20)
+            pins.analogWritePin(AnalogPin.P16, 180)
+        } else {
+            pins.analogWritePin(AnalogPin.P8, 180)
+            pins.analogWritePin(AnalogPin.P16, 180)
+        }
     } else {
-        pins.analogWritePin(AnalogPin.P12, 512)
-        pins.analogWritePin(AnalogPin.P16, 512)
+        pins.analogWritePin(AnalogPin.P8, 0)
+        pins.analogWritePin(AnalogPin.P16, 0)
     }
 })
